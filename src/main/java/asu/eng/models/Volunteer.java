00a99335f6ba696538fe96aeb902ac9c04250c22
mohamed khaled <momoelazhary@gmail.com>
2024-11-17 19:48:29 +0200

    Revert "Volunteer class modified"
    This reverts commit 8768c0b7c1578843cba40a405e5b1dbb35fefe4f.

--- src/main/java/asu/eng/models/Volunteer.java
+++ src/main/java/asu/eng/models/Volunteer.java
@@ -2,27 +2,17 @@ package asu.eng.models;
 
 public class Volunteer {
     private VolunteerReservation reservation;
-    private VolunteerReservation[] allReservations;
+    private User previousUserAddOn;
     private boolean eventDateModified;
     private Event event;
-    private static databaseObject dbObject;
 
+    public Volunteer(User previousUserAddOn) {
+        // make a database request here to fetch Visitor
 
-    public Volunteer(int id) {
-        super(dbObject = getDBObject(id));
-
-
-//        this.reservation = // use dbObject to get the data of normalVisit field
-
-
+        this.previousUserAddOn = previousUserAddOn;
     }
 
-    private static databaseObject getDBObject(int id) {
-        // make a database request here to fetch Volunteer
-//        return object of databaseObject type;
-    }
-
-    public boolean makeReservation(int eventId) {
+    public VolunteerReservation makeReservation(int eventId) {
 
     }
 
@@ -30,24 +20,8 @@ public class Volunteer {
 
     }
 
-    public VolunteerReservation[] getAllReservations() {
-
-    }
-
     public boolean cancelReservation(int reservationId) {
 
     }
 
-
-    public static boolean create(String name, int age) {
-
-    }
-
-    public static boolean update(int id, String name, int age) {
-
-    }
-
-    public static boolean delete(int id) {
-
-    }
 }
