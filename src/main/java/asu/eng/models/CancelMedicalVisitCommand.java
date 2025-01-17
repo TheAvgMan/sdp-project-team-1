5c8ec29eb6b60b66883f136cf44a25131f21d27c
Hazem Zaki <hazemzaki.work@gmail.com>
2025-01-17 17:33:09 +0200

    Add Command Design Pattern

---
+++ src/main/java/asu/eng/models/CancelMedicalVisitCommand.java
@@ -0,0 +1,14 @@
+package asu.eng.models;
+
+public class CancelMedicalVisitCommand implements ICommand {
+    private Doctor doctor;
+    private String visitId;
+    public CancelMedicalVisitCommand(Doctor doctor, String visitId) {
+        this.doctor = doctor;
+        this.visitId = visitId;
+    }
+    @Override
+    public void execute() {
+        doctor.cancelMedicalVisit(visitId);
+    }
+}
---
+++ src/main/java/asu/eng/models/Scheduler.java
@@ -0,0 +1,17 @@
+package asu.eng.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scheduler {
+    private List<ICommand> commandList = new ArrayList<>();
+    public void takeCommand(ICommand command) {
+        commandList.add(command);
+    }
+    public void executeCommands() {
+        for (ICommand command : commandList){
+            command.execute();
+        }
+        commandList.clear();
+    }
+}
